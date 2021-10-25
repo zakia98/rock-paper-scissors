dc5e7f168eab4de32c55a63d9f58dfd3b104a1d2
ameen <ameenahmed1998@gmail.com>
2021-10-25 13:01:11 +0100

    finished project

--- js-file2.js
+++ js-file2.js
@@ -45,7 +45,7 @@ function addHistory(result, finalMessage, playerSelection, computerSelection){
     records.classList.add('records');
     
     const h3 = document.createElement('h3');
-    h3.textContent = `${result}`
+    h3.textContent = `Round: ${round}! ${result}!`
 
     const h4 = document.createElement('h4');
     h4.textContent = `${finalMessage}`
@@ -60,31 +60,57 @@ function addHistory(result, finalMessage, playerSelection, computerSelection){
     record.appendChild(records)
 }
 
+function changeAnnoucement(round, result, playerSelection, computerSelection) {
+    const announcement = document.querySelector('#result');
+    announcement.textContent = `Round ${round}: You ${result}` +
+        `\nYou chose: ${playerSelection}` +
+        `\nComputer chose: ${computerSelection}`
+}
+
+function limitHistory() {
+    const records = document.querySelector('#record');
+    console.log(record.childElementCount)
+    if (records.childElementCount > 4) {
+        let firstChild = records.firstElementChild
+        records.removeChild(firstChild)
+    } 
+}
+
+let round = 0
 
 
 const rockbtn = document.querySelector('#rock');
 rockbtn.addEventListener('click', () => {
+    round++
     let computerSelection = computerPlay(3)
     const playerSelection = 'Rock'
     result = playRound(playerSelection, computerSelection)
-    finalMessage = whyResult(result, 'Rock', computerSelection)
+    finalMessage = whyResult(result, playerSelection, computerSelection)
+    changeAnnoucement(round, result, playerSelection, computerSelection)
+    limitHistory()
     addHistory(result, finalMessage, playerSelection, computerSelection)
 })
 
 const paperbtn = document.querySelector('#paper')
 paperbtn.addEventListener('click',() => {
+    round++
     let computerSelection = computerPlay(3)
     const playerSelection = 'Paper'
     result = playRound(playerSelection, computerSelection)
     finalMessage = whyResult(result, playerSelection, computerSelection)
+    changeAnnoucement(round, result, playerSelection, computerSelection)
+    limitHistory()
     addHistory(result, finalMessage, playerSelection, computerSelection)
 })
 
 const scissorsbtn = document.querySelector('#scissors')
 scissorsbtn.addEventListener('click', () => {
+    round++
     let computerSelection = computerPlay(3)
     const playerSelection = 'Scissors'
     result = playRound(playerSelection, computerSelection)
     finalMessage = whyResult(result, playerSelection, computerSelection)
+    changeAnnoucement(round, result, playerSelection, computerSelection)
+    limitHistory()
     addHistory(result, finalMessage, playerSelection, computerSelection)
 })
